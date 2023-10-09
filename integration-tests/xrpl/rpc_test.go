34c19cd9e6af59c7ecb8b09152fdc1d32985ddd9
Dzmitry Hil <dzmitryhil@gmail.com>
2023-10-09 15:52:21 +0300

    Add XRPL low/high amounts payment test. (#27)

--- integration-tests/xrpl/rpc_test.go
+++ integration-tests/xrpl/rpc_test.go
@@ -5,6 +5,7 @@ package xrpl_test
 
 import (
 	"context"
+	"fmt"
 	"testing"
 
 	rippledata "github.com/rubblelabs/ripple/data"
@@ -443,7 +444,7 @@ func TestMultisigWithMasterKeyRemoval(t *testing.T) {
 			Account:         multisigAccToDisable,
 			TransactionType: rippledata.ACCOUNT_SET,
 		},
-		SetFlag: lo.ToPtr(uint32(4)),
+		SetFlag: lo.ToPtr(uint32(rippledata.TxSetDisableMaster)),
 	}
 	require.NoError(t, chains.XRPL.AutoFillSignAndSubmitTx(ctx, t, &disableMasterKeyTx, multisigAccToDisable))
 	t.Logf("The master key is disabled")
@@ -506,6 +507,239 @@ func TestAccountTx(t *testing.T) {
 	accountTxRes, err := chains.XRPL.RPCClient().AccountTx(ctx, senderAcc, -1, -1, nil)
 	require.NoError(t, err)
 	require.Len(t, accountTxRes.Transactions, 5) // faucet send + 4 more
+}
+
+func TestXRPLHighLowAmountsPayments(t *testing.T) {
+	t.Parallel()
+
+	ctx, chains := integrationtests.NewTestingContext(t)
+
+	tests := []struct {
+		name                      string
+		senderBalanceBefore       string
+		recipientBalanceBefore    string
+		sendingAmounts            []string
+		sendingBackAmount         string
+		wantSenderBalanceAfter    string
+		wantRecipientBalanceAfter string
+		wantDeliveredAmounts      []string
+	}{
+		{
+			name:                   "normal_send",
+			senderBalanceBefore:    "1000.0",
+			recipientBalanceBefore: "1000.0",
+			sendingAmounts:         []string{"0.0001"},
+
+			wantSenderBalanceAfter:    "999.9999",
+			wantRecipientBalanceAfter: "1000.0001",
+			wantDeliveredAmounts:      []string{"0.0001"},
+		},
+		{
+			name:                      "low_sender_high_recipient_balance_low_amount_sending",
+			senderBalanceBefore:       "10",
+			recipientBalanceBefore:    "1e17",
+			sendingAmounts:            []string{"10"},
+			wantSenderBalanceAfter:    "0",
+			wantRecipientBalanceAfter: "1e17",
+			wantDeliveredAmounts:      []string{"10"}, // the delivery amount says that 10 is delivered but in reality zero
+		},
+		{
+			name:                      "high_sender_low_recipient_balance_low_amount_sending",
+			senderBalanceBefore:       "1e17",
+			recipientBalanceBefore:    "0",
+			sendingAmounts:            []string{"10"},
+			wantSenderBalanceAfter:    "1e17",
+			wantRecipientBalanceAfter: "10", // the sender balance remains the same, but recipient has received tokens
+			wantDeliveredAmounts:      []string{"10"},
+		},
+		{
+			name:                      "high_sender_low_recipient_balance_low_amount_sending_multiple_sum_100",
+			senderBalanceBefore:       "1e17",
+			recipientBalanceBefore:    "0",
+			sendingAmounts:            []string{"49", "49", "2"},
+			wantSenderBalanceAfter:    "1e17",
+			wantRecipientBalanceAfter: "100", // the sender balance remains the same, but recipient has received tokens
+			wantDeliveredAmounts:      []string{"49", "49", "2"},
+		},
+		{
+			name:                      "high_sender_low_recipient_balance_low_amount_sending_single_100",
+			senderBalanceBefore:       "1e17",
+			recipientBalanceBefore:    "0",
+			sendingAmounts:            []string{"100"},
+			wantSenderBalanceAfter:    "999999999999999e2", // the test case in sum  similar to previous but affects the balance
+			wantRecipientBalanceAfter: "100",
+			wantDeliveredAmounts:      []string{"100"},
+		},
+		{
+			name:                   "high_sender_low_recipient_balance_low_amount_sending_many_low_amounts_with_high_sum_and_sending_back",
+			senderBalanceBefore:    "1e17",
+			recipientBalanceBefore: "0",
+			// !!! the test works with 99 on the localnet, but not on the testnet for some reason !!!
+			sendingAmounts: lo.Times(21, func(index int) string {
+				return "49"
+			}),
+			sendingBackAmount:         "1029",
+			wantSenderBalanceAfter:    "100000000000001e3", // 100000000000001e3 - 1e17 = 1000, so we have minted `1000` tokens
+			wantRecipientBalanceAfter: "0",
+			wantDeliveredAmounts: lo.Times(21, func(index int) string { // the total delivered amount completely incorrect
+				return "49"
+			}),
+		},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			issuerAcc := chains.XRPL.GenAccount(ctx, t, 1)
+			t.Logf("Issuer account: %s", issuerAcc)
+			senderAcc := chains.XRPL.GenAccount(ctx, t, 1)
+			t.Logf("Sender account: %s", senderAcc)
+			recipientAcc := chains.XRPL.GenAccount(ctx, t, 1)
+			t.Logf("Recipient account: %s", recipientAcc)
+
+			// Enable rippling on this account's trust lines by default.
+			defaultRippleAccountSetTx := rippledata.AccountSet{
+				SetFlag: lo.ToPtr(uint32(rippledata.TxDefaultRipple)),
+				TxBase: rippledata.TxBase{
+					TransactionType: rippledata.ACCOUNT_SET,
+				},
+			}
+			require.NoError(t, chains.XRPL.AutoFillSignAndSubmitTx(ctx, t, &defaultRippleAccountSetTx, issuerAcc))
+
+			currency, err := rippledata.NewCurrency("FOO")
+			require.NoError(t, err)
+			currencyTrustSetValue, err := rippledata.NewValue("10e80", false)
+			require.NoError(t, err)
+
+			// all allow the coin to be consumed by and sender and recipient
+			for _, acc := range []rippledata.Account{senderAcc, recipientAcc} {
+				fooCurrencyTrustSetTx := rippledata.TrustSet{
+					LimitAmount: rippledata.Amount{
+						Value:    currencyTrustSetValue,
+						Currency: currency,
+						Issuer:   issuerAcc,
+					},
+					TxBase: rippledata.TxBase{
+						TransactionType: rippledata.TRUST_SET,
+					},
+				}
+				require.NoError(t, chains.XRPL.AutoFillSignAndSubmitTx(ctx, t, &fooCurrencyTrustSetTx, acc))
+			}
+
+			// fill initial sender balance
+			if tt.senderBalanceBefore != "0" {
+				senderBalanceBeforeAmount, err := rippledata.NewValue(tt.senderBalanceBefore, false)
+				require.NoError(t, err)
+				issuerToSenderPaymentTx := rippledata.Payment{
+					Destination: senderAcc,
+					Amount: rippledata.Amount{
+						Value:    senderBalanceBeforeAmount,
+						Currency: currency,
+						Issuer:   issuerAcc,
+					},
+					TxBase: rippledata.TxBase{
+						TransactionType: rippledata.PAYMENT,
+					},
+				}
+				require.NoError(t, chains.XRPL.AutoFillSignAndSubmitTx(ctx, t, &issuerToSenderPaymentTx, issuerAcc))
+			}
+
+			// fill initial recipient balance
+			if tt.recipientBalanceBefore != "0" {
+				recipientBalanceBeforeAmount, err := rippledata.NewValue(tt.recipientBalanceBefore, false)
+				require.NoError(t, err)
+				issuerToRecipientPaymentTx := rippledata.Payment{
+					Destination: recipientAcc,
+					Amount: rippledata.Amount{
+						Value:    recipientBalanceBeforeAmount,
+						Currency: currency,
+						Issuer:   issuerAcc,
+					},
+					TxBase: rippledata.TxBase{
+						TransactionType: rippledata.PAYMENT,
+					},
+				}
+				require.NoError(t, chains.XRPL.AutoFillSignAndSubmitTx(ctx, t, &issuerToRecipientPaymentTx, issuerAcc))
+			}
+
+			senderBalanceBefore := getBalanceAccount(ctx, t, chains.XRPL, senderAcc, issuerAcc, currency)
+			recipientBalanceBefore := getBalanceAccount(ctx, t, chains.XRPL, recipientAcc, issuerAcc, currency)
+
+			deliveredAmounts := make([]string, 0, len(tt.sendingAmounts))
+			for _, sendingAmount := range tt.sendingAmounts {
+				sendingAmount, err := rippledata.NewValue(sendingAmount, false)
+				require.NoError(t, err)
+				senderToRecipientPaymentTx := rippledata.Payment{
+					Destination: recipientAcc,
+					Amount: rippledata.Amount{
+						Value:    sendingAmount,
+						Currency: currency,
+						Issuer:   issuerAcc,
+					},
+					TxBase: rippledata.TxBase{
+						TransactionType: rippledata.PAYMENT,
+					},
+				}
+				require.NoError(t, chains.XRPL.AutoFillSignAndSubmitTx(ctx, t, &senderToRecipientPaymentTx, senderAcc))
+				// fetch account transactions
+				txWithMetaData, err := chains.XRPL.RPCClient().Tx(ctx, *senderToRecipientPaymentTx.GetHash())
+				require.NoError(t, err)
+				deliveredAmount := txWithMetaData.MetaData.DeliveredAmount.Value.String()
+				deliveredAmounts = append(deliveredAmounts, deliveredAmount)
+			}
+
+			if tt.sendingBackAmount != "" && tt.sendingBackAmount != "0" {
+				sendingAmount, err := rippledata.NewValue(tt.sendingBackAmount, false)
+				require.NoError(t, err)
+				senderToRecipientPaymentTx := rippledata.Payment{
+					Destination: senderAcc,
+					Amount: rippledata.Amount{
+						Value:    sendingAmount,
+						Currency: currency,
+						Issuer:   issuerAcc,
+					},
+					TxBase: rippledata.TxBase{
+						TransactionType: rippledata.PAYMENT,
+					},
+				}
+				require.NoError(t, chains.XRPL.AutoFillSignAndSubmitTx(ctx, t, &senderToRecipientPaymentTx, recipientAcc))
+			}
+
+			senderBalanceAfter := getBalanceAccount(ctx, t, chains.XRPL, senderAcc, issuerAcc, currency)
+			recipientBalanceAfter := getBalanceAccount(ctx, t, chains.XRPL, recipientAcc, issuerAcc, currency)
+
+			t.Logf(
+				"Sender before: %s | Recipient before: %s | Sending amounts: %v | Sender after: %s | Recipient after: %s | Delivered amount: %v",
+				senderBalanceBefore, recipientBalanceBefore, tt.sendingAmounts, senderBalanceAfter, recipientBalanceAfter, deliveredAmounts,
+			)
+
+			require.Equal(t, tt.wantSenderBalanceAfter, senderBalanceAfter)
+			require.Equal(t, tt.wantRecipientBalanceAfter, recipientBalanceAfter)
+			require.Equal(t, tt.wantDeliveredAmounts, deliveredAmounts)
+		})
+	}
+}
+
+func getBalanceAccount(
+	ctx context.Context,
+	t *testing.T,
+	xrplChain integrationtests.XRPLChain,
+	acc,
+	issuer rippledata.Account,
+	currency rippledata.Currency,
+) string {
+	t.Helper()
+	issuerCurrencyKey := fmt.Sprintf("%s/%s", currency.String(), issuer.String())
+	balance, ok := xrplChain.GetAccountBalances(ctx, t, acc)[issuerCurrencyKey]
+	if !ok {
+		return "0"
+	}
+	if balance.Value == nil {
+		return "0"
+	}
+
+	return balance.Value.String()
 }
 
 func buildXrpPaymentTxForMultiSigning(
