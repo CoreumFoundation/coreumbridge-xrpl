947cb19a49c0157d8d0baa4472839cffe497aad3
keyne <kleuyckx@gmail.com>
2023-10-11 13:07:06 +0100

    lint errors

--- contract/src/error.rs
+++ contract/src/error.rs
@@ -23,22 +23,18 @@ pub enum ContractError {
     CoreumTokenAlreadyRegistered { denom: String },
 
     #[error(
-    "XRPLTokenAlreadyRegistered: Token with issuer: {} and currency: {} is already registered",
-    issuer,
-    currency
+        "XRPLTokenAlreadyRegistered: Token with issuer: {} and currency: {} is already registered",
+        issuer,
+        currency
     )]
     XRPLTokenAlreadyRegistered { issuer: String, currency: String },
 
     #[error("InvalidFundsAmount: Need to send exactly the issue fee amount")]
     InvalidFundsAmount {},
 
-<<<<<<< HEAD
-    #[error("RegistrationFailure: Currency/denom generated already exists, please try again")]
-=======
     #[error(
-    "RegistrationFailure: Random currency/denom generated already exists, please try again"
+        "RegistrationFailure: Random currency/denom generated already exists, please try again"
     )]
->>>>>>> master
     RegistrationFailure {},
 
     #[error("UnauthorizedSender: Sender is not a valid relayer")]
@@ -51,7 +47,7 @@ pub enum ContractError {
     OperationAlreadyExecuted {},
 
     #[error(
-    "EvidenceAlreadyProvided: The relayer already provided its evidence for the operation"
+        "EvidenceAlreadyProvided: The relayer already provided its evidence for the operation"
     )]
     EvidenceAlreadyProvided {},
 
