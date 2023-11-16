12d661b611e7813b80a3c38af81aac80cf4a7150
Dzmitry Hil <dzmitryhil@gmail.com>
2023-11-16 16:02:27 +0300

    Add XRPL tx finality check.

--- relayer/xrpl/constatns.go
+++ relayer/xrpl/constatns.go
@@ -4,12 +4,16 @@ const (
 	// TecPathDryTxResult defines that provided paths did not have enough liquidity to send anything at all.
 	//	This could mean that the source and destination accounts are not linked by trust lines.
 	TecPathDryTxResult = "tecPATH_DRY"
-	// TefNOTicketTxResult defines the result which indicates the usage of the passed ticket or not created ticket.
+	// TefNOTicketTxResult defines that the usage of the passed ticket or not created ticket.
 	TefNOTicketTxResult = "tefNO_TICKET"
-	// TefPastSeqTxResult defines the result which indicates the usage of the sequence in the past.
+	// TefPastSeqTxResult defines that the usage of the sequence in the past.
 	TefPastSeqTxResult = "tefPAST_SEQ"
-	// TerPreSeqTxResult defines the result which indicates the usage of the sequence in the future.
-	TerPreSeqTxResult = "terPRE_SEQ"
-	// TecInsufficientReserveTxResult defines the result which indicates the insufficient reserve to complete requested operation.
+	// TefMaxLedgerTxResult defines that ledger sequence too high.
+	TefMaxLedgerTxResult = "tefMAX_LEDGER"
+	// TecInsufficientReserveTxResult defines that reserve is insufficient to complete requested operation.
 	TecInsufficientReserveTxResult = "tecINSUFFICIENT_RESERVE"
+	// TecTxResultPrefix is `tec` prefix for tx result.
+	TecTxResultPrefix = "tec"
+	// TemTxResultPrefix is `tem` prefix for tx result.
+	TemTxResultPrefix = "tem"
 )
