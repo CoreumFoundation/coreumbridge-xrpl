5503731d7283119c73028abe5e28f75ce45e9f67
milad <mza1989@gmail.com>
2024-01-01 10:14:34 +0330

    changed keyring default home (#78)
    * changed keyring default home
    
    * Merge branch 'master' into milad/change-keyring-home
    
    * Merge branch 'master' into milad/change-keyring-home

--- relayer/cmd/cli/cli.go
+++ relayer/cmd/cli/cli.go
@@ -4,7 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"path"
+	"path/filepath"
 	"strconv"
 	"strings"
 
@@ -27,9 +27,21 @@ import (
 	"github.com/CoreumFoundation/coreumbridge-xrpl/relayer/xrpl"
 )
 
-const (
+func init() {
+	userHomeDir, err := os.UserHomeDir()
+	if err != nil {
+		panic(err)
+	}
+
+	DefaultHomeDir = filepath.Join(userHomeDir, ".coreumbridge-xrpl-relayer")
+}
+
+var (
 	// DefaultHomeDir is default home for the relayer.
-	DefaultHomeDir = ".coreumbridge-xrpl-relayer"
+	DefaultHomeDir string
+)
+
+const (
 	// FlagHome is home flag.
 	FlagHome = "home"
 	// FlagKeyName is key name flag.
@@ -838,30 +850,7 @@ func getRelayerHomeRunnerConfig(cmd *cobra.Command) (runner.Config, error) {
 }
 
 func getRelayerHome(cmd *cobra.Command) (string, error) {
-	home, err := cmd.Flags().GetString(FlagHome)
-	if err != nil {
-		return "", errors.WithStack(err)
-	}
-	if home == "" || home == DefaultHomeDir {
-		home, err = getUserHomeDir(DefaultHomeDir)
-		if err != nil {
-			return "", err
-		}
-	}
-
-	return home, nil
-}
-
-func getUserHomeDir(subPath ...string) (string, error) {
-	dirname, err := os.UserHomeDir()
-	if err != nil {
-		return "", errors.Wrap(err, "failed to get user home dir")
-	}
-	for _, item := range subPath {
-		dirname = path.Join(dirname, item)
-	}
-
-	return dirname, nil
+	return cmd.Flags().GetString(FlagHome)
 }
 
 func addHomeFlag(cmd *cobra.Command) {
